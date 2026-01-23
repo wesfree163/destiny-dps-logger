389cc4500001921c0c216bef0143bd97087df26c
Wesley Freeman <22487192+wesfree163@users.noreply.github.com>
2026-01-23 12:10:58 -0500

    Add files via upload

---
+++ codeMisc/D2exp_chain.py
@@ -0,0 +1,29 @@
+import keyboard
+import time
+
+# import os
+import sys
+import numpy
+import itertools
+import threading
+from pywinauto import mouse
+from pywinauto.keyboard import send_keys
+
+from skimage.io import imread #type: ignore
+from sklearn.svm import SVC #type: ignore
+from sklearn.metrics import accuracy_score #type: ignore
+from skimage.transform import resize #type: ignore
+from sklearn.model_selection import train_test_split #type: ignore
+from time import sleep as stop
+# from pywinauto import keyboard
+
+
+
+import os
+import numpy as np
+from skimage.io import imread
+from skimage.transform import resize
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+
